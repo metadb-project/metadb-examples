9b4a923b35272e9b43d6025dbeac444836248020
Nassib Nassar <nrn@etymon.com>
2023-10-10 12:41:37 -0400

    Add LDP version of count_loans report

---
+++ folio/ldp/reports/count_loans/count_loans.sql
@@ -0,0 +1,20 @@
+--ldp:function count_loans
+
+DROP FUNCTION IF EXISTS count_loans
+
+CREATE FUNCTION count_loans(
+    start_date date DEFAULT '1000-01-01',
+    end_date date DEFAULT '3000-01-01')
+RETURNS TABLE(
+    item_id uuid,
+    loan_count integer)
+AS $$
+SELECT item_id::uuid,
+       count(*) AS loan_count
+    FROM public.circulation_loans
+    WHERE start_date <= loan_date AND loan_date < end_date
+    GROUP BY item_id
+$$
+LANGUAGE SQL
+STABLE
+PARALLEL SAFE;
